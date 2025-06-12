f001803aae4f530977653e05f257dcffb9554f85
harsh <harshp.ug23.cs@nitp.ac.in>
2025-06-12 12:14:19 +0530

    Make SidebarLayout as a Humburger Menu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Sidebar.module.css'
 const CHATS = [
   {
@@ -23,7 +24,14 @@ const CHATS = [
 ];
 
 export function Sidebar({chats=CHATS,activechatid=1}){
-   return <div className={styles.Sidebar}>
+  const[isOpen,setisOpen]=useState(false);
+  function sethandletoggle(){
+    setisOpen(!isOpen);
+  }
+   return (
+   <>
+   <button className={styles.MenuButton} onClick={sethandletoggle}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></button>
+   <div className={styles.Sidebar} data-open={isOpen}>
     <ul className={styles.Chats}>
         {chats.map((chat) =>(
             <li key={chat.id} className={styles.Chat} data-active={chat.id===activechatid}><button className={styles.chatbutton}><div className={styles.chattitle}>{chat.title}</div></button></li>
@@ -31,4 +39,8 @@ export function Sidebar({chats=CHATS,activechatid=1}){
         }
     </ul>
    </div>
+   {isOpen && <div className={styles.overlay} onClick={sethandletoggle}/>}
+   </>
+   
+   )
 }
